006cb3e3b16cfefa6b83fdde3e8ae869d1bad0e5
Scusemua <benrcarver@gmail.com>
2026-03-20 20:56:07 -0400

    Fixed compile-time bug

--- Patches/NetworkManagerPatches.cs
+++ Patches/NetworkManagerPatches.cs
@@ -962,7 +962,7 @@ namespace IssaPlugin.Patches
                     msg.Position,
                     Quaternion.identity
                 );
-                vfx.transform.localScale = Vector3.one * msg.NukeExplosionVfxScale.Value;
+                vfx.transform.localScale = Vector3.one * msg.NukeExplosionVfxScale;
                 Object.Destroy(vfx, Configuration.NukeExplosionVfxDuration.Value);
             }
             else
@@ -972,7 +972,7 @@ namespace IssaPlugin.Patches
                     VfxType.RocketLauncherRocketExplosion,
                     msg.Position,
                     Quaternion.identity,
-                    Vector3.one * msg.NukeExplosionScale.Value
+                    Vector3.one * msg.NukeExplosionVfxScale
                 );
             }
 
