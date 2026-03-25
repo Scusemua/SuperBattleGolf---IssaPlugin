163ada0a38b2106a4144798c8d03ae7785af502c
Scusemua <benrcarver@gmail.com>
2026-03-25 18:23:57 -0400

    Incremented version numbers

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -4,6 +4,6 @@ namespace IssaPlugin
     {
         public const string PLUGIN_GUID = "com.scusemua.IssaPlugin";
         public const string PLUGIN_NAME = "IssaPlugin";
-        public const string PLUGIN_VERSION = "0.0.13";
+        public const string PLUGIN_VERSION = "0.0.14";
     }
 }
